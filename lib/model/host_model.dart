dd3a4a63d9487d879fe244d4ffaaefa97dd2189d
minimissile <minimissile888@gmail.com>
2024-04-30 17:43:54 +0800

    1

--- lib/model/host_model.dart
+++ lib/model/host_model.dart
@@ -1,67 +1,203 @@
+/// 主机监控响应模型类
+class HostResponseModel {
+  List<HostModel>? content;
+  Pageable? pageable;
+  bool? last;
+  int? totalPages;
+  int? totalElements;
+  int? number;
+  Sort? sort;
+  int? size;
+  int? numberOfElements;
+  bool? first;
+  bool? empty;
 
-/// 主机模型类
-class HostModel {
-  String? username;
-  String? password;
-  String? avatar;
-  String? registerTime;
-  String? nickName;
-  String? updateTime;
-  String? lastLoginIp;
-  String? lastLoginTime;
-  String? token;
-  String? uid;
-  int? phoneNumber;
-  int? userLevel;
-  String? email;
-
-  HostModel({
-    this.username,
-    this.password,
-    this.avatar,
-    this.registerTime,
-    this.nickName,
-    this.updateTime,
-    this.lastLoginIp,
-    this.token,
-    this.lastLoginTime,
-    this.uid,
-    this.phoneNumber,
-    this.userLevel,
-    this.email,
+  HostResponseModel({
+    this.content,
+    this.pageable,
+    this.last,
+    this.totalPages,
+    this.totalElements,
+    this.number,
+    this.sort,
+    this.size,
+    this.numberOfElements,
+    this.first,
+    this.empty,
   });
 
-  HostModel.fromJson(Map<String, dynamic> json) {
-    username = json['username'];
-    password = json['password'];
-    avatar = json['avatar'];
-    registerTime = json['registerTime'];
-    nickName = json['nickName'];
-    updateTime = json['updateTime'];
-    lastLoginIp = json['lastLoginIp'];
-    token = json['token'];
-    lastLoginTime = json['lastLoginTime'];
-    uid = json['uid'];
-    phoneNumber = json['phoneNumber'];
-    userLevel = json['userLevel'];
-    email = json['email'];
+  HostResponseModel.fromJson(Map<String, dynamic> json) {
+    if (json['content'] != null) {
+      content = <HostModel>[];
+      json['content'].forEach((v) {
+        content!.add(HostModel.fromJson(v));
+      });
+    }
+    pageable = json['pageable'] != null ? Pageable.fromJson(json['pageable']) : null;
+    last = json['last'];
+    totalPages = json['totalPages'];
+    totalElements = json['totalElements'];
+    number = json['number'];
+    sort = json['sort'] != null ? Sort.fromJson(json['sort']) : null;
+    size = json['size'];
+    numberOfElements = json['numberOfElements'];
+    first = json['first'];
+    empty = json['empty'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['username'] = username;
-    data['password'] = password;
-    data['avatar'] = avatar;
-    data['registerTime'] = registerTime;
-    data['nickName'] = nickName;
-    data['updateTime'] = updateTime;
-    data['lastLoginIp'] = lastLoginIp;
-    data['token'] = token;
-    data['lastLoginTime'] = lastLoginTime;
-    data['uid'] = uid;
-    data['phoneNumber'] = phoneNumber;
-    data['userLevel'] = userLevel;
-    data['email'] = email;
+    if (content != null) {
+      data['content'] = content!.map((v) => v.toJson()).toList();
+    }
+    if (pageable != null) {
+      data['pageable'] = pageable!.toJson();
+    }
+    data['last'] = last;
+    data['totalPages'] = totalPages;
+    data['totalElements'] = totalElements;
+    data['number'] = number;
+    if (sort != null) {
+      data['sort'] = sort!.toJson();
+    }
+    data['size'] = size;
+    data['numberOfElements'] = numberOfElements;
+    data['first'] = first;
+    data['empty'] = empty;
+    return data;
+  }
+}
+
+/// 主机模型类
+class HostModel {
+  int? id;
+  String? title;
+  String? cpu;
+  String? ram;
+  String? disk;
+  String? ip;
+  String? location;
+  String? bandwidth;
+  String? price;
+  String? productUrl;
+  String? promoCode;
+  String? remark;
+  int? monitorStatus;
+  int? flag;
+  String? monitorTime;
+  int? level;
+
+  HostModel({
+    this.id,
+    this.title,
+    this.cpu,
+    this.ram,
+    this.disk,
+    this.ip,
+    this.location,
+    this.bandwidth,
+    this.price,
+    this.productUrl,
+    this.promoCode,
+    this.remark,
+    this.monitorStatus,
+    this.flag,
+    this.monitorTime,
+    this.level,
+  });
+
+  HostModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    cpu = json['cpu'];
+    ram = json['ram'];
+    disk = json['disk'];
+    ip = json['ip'];
+    location = json['location'];
+    bandwidth = json['bandwidth'];
+    price = json['price'];
+    productUrl = json['productUrl'];
+    promoCode = json['promoCode'];
+    remark = json['remark'];
+    monitorStatus = json['monitorStatus'];
+    flag = json['flag'];
+    monitorTime = json['monitorTime'];
+    level = json['level'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['cpu'] = cpu;
+    data['ram'] = ram;
+    data['disk'] = disk;
+    data['ip'] = ip;
+    data['location'] = location;
+    data['bandwidth'] = bandwidth;
+    data['price'] = price;
+    data['productUrl'] = productUrl;
+    data['promoCode'] = promoCode;
+    data['remark'] = remark;
+    data['monitorStatus'] = monitorStatus;
+    data['flag'] = flag;
+    data['monitorTime'] = monitorTime;
+    data['level'] = level;
+    return data;
+  }
+}
+
+class Pageable {
+  Sort? sort;
+  int? offset;
+  int? pageNumber;
+  int? pageSize;
+  bool? paged;
+  bool? unpaged;
+
+  Pageable({this.sort, this.offset, this.pageNumber, this.pageSize, this.paged, this.unpaged});
+
+  Pageable.fromJson(Map<String, dynamic> json) {
+    sort = json['sort'] != null ? Sort.fromJson(json['sort']) : null;
+    offset = json['offset'];
+    pageNumber = json['pageNumber'];
+    pageSize = json['pageSize'];
+    paged = json['paged'];
+    unpaged = json['unpaged'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (sort != null) {
+      data['sort'] = sort!.toJson();
+    }
+    data['offset'] = offset;
+    data['pageNumber'] = pageNumber;
+    data['pageSize'] = pageSize;
+    data['paged'] = paged;
+    data['unpaged'] = unpaged;
+    return data;
+  }
+}
+
+class Sort {
+  bool? sorted;
+  bool? unsorted;
+  bool? empty;
+
+  Sort({this.sorted, this.unsorted, this.empty});
+
+  Sort.fromJson(Map<String, dynamic> json) {
+    sorted = json['sorted'];
+    unsorted = json['unsorted'];
+    empty = json['empty'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['sorted'] = sorted;
+    data['unsorted'] = unsorted;
+    data['empty'] = empty;
     return data;
   }
 }
--- lib/pages/host_monitor/host_monitor_page.dart
+++ lib/pages/host_monitor/host_monitor_page.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:sideline_group/model/host_model.dart';
 import 'package:sideline_group/utils/logger_util.dart';
 import 'package:sideline_group/widgets/appbar_widget.dart';
+import 'package:sideline_group/widgets/custom_refresher_widget.dart';
 
 /// 主机监控
 class HostMonitorPage extends StatefulWidget {
@@ -13,36 +15,50 @@ class HostMonitorPage extends StatefulWidget {
 }
 
 class _HostMonitorPageState extends State<HostMonitorPage> {
-  Map<String, dynamic> params = {"pageNo": 1, "pageSize": 100};
+  /// 滚动控制器
+  final ScrollController _scrollController = ScrollController();
+
+  /// 刷新控制器
+  final RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  Map<String, dynamic> params = {"pageNo": 1, "pageSize": 200};
 
   List<HostModel> list = [];
 
-  void _getDataList() async {
+  Future<void> _getDataList({bool isLoadMore = false}) async {
     String url = 'https://backend.stock.hostmonit.com/monitor/findMonitorLess';
-
-    Map<String, dynamic> headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer your_token_here', // 添加需要的授权头
-    };
-
     Dio dio = Dio();
 
     try {
-      Response response = await dio.post(
-        url,
-        options: Options(headers: headers), // 添加 headers
-      );
+      Response response = await dio.post(url, data: params);
 
+      var res = HostResponseModel.fromJson(response.data);
+      var tempList = res.content ?? [];
 
-      // 处理响应数据
-      print('Response data: ${response.data}');
+      // 是否为加载更多
+      if (isLoadMore) {
+        tempList = [...list, ...tempList];
+      }
 
-
+      setState(() {
+        list.clear();
+        list = tempList;
+      });
     } catch (e) {
-      Logger.log(e.toString());
+      Logger.log("获取主机列表出错：${e.toString()}");
     }
+  }
 
-    dio.post(url);
+  /// 加载中事件
+  void _onLoading() async {
+    await _getDataList(isLoadMore: true);
+  }
+
+  /// 刷新事件
+  void _onRefresh() async {
+    await _getDataList();
+    _refreshController.refreshCompleted(resetFooterState: true);
+    setState(() {});
   }
 
   @override
@@ -52,16 +68,108 @@ class _HostMonitorPageState extends State<HostMonitorPage> {
   }
 
   @override
+  void dispose() {
+    _refreshController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(
-        title: '主机监控',
-      ),
-      body: Container(
-        child: Column(
-          children: [],
-        ),
+      appBar: const AppBarWidget(title: '主机监控'),
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomRefresher(
+              itemCount: list.length,
+              tipsColor: Colors.black38,
+              scrollController: _scrollController,
+              showScrollbar: true,
+              controller: _refreshController,
+              onRefresh: _onRefresh,
+              onLoading: _onLoading,
+              child: ListView.separated(
+                controller: _scrollController,
+                padding: const EdgeInsets.all(15).copyWith(bottom: 0),
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  final hostData = list[index];
+                  return _buildHostItem(data: hostData);
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 10,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Widget _buildHostItem({required HostModel data}) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.12), // 阴影颜色
+            spreadRadius: 5, // 阴影扩散程度
+            blurRadius: 10, // 阴影模糊程度
+            offset: const Offset(0, 3), // 阴影偏移
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          rowCell(title: 'title', value: data.title ?? '--'),
+          rowCell(title: 'location', value: data.location ?? '--'),
+          rowCell(title: 'level', value: (data.level ?? 1).toString()),
+        ],
+      ),
+    );
+  }
+}
+
+Widget rowCell({
+  required String title,
+  required String value,
+}) {
+  return Container(
+    margin: const EdgeInsets.only(bottom: 3),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 70,
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w700,
+              color: Colors.black87,
+              height: 18 / 12,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.black54,
+              height: 18 / 12,
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
